22336b5a32bb1cf6f2e8241df8e317ce58a8254f
Minji Choi <minj22222c@gmail.com>
2023-09-14 11:58:07 +0900

    모듈 진행 중

---
+++ src/module.js
@@ -0,0 +1,18 @@
+// js:es-module 방법에서 모듈을 내보내는 방법
+// - 내보내고 싶은 모듈앞에 export를 쓰면 됨
+// 모듈: 코드집합, 변수, 함수, 객체, 클래스, 인터페이스 등
+
+// export(수출): 내보내기
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+export const appName = "myapp";
+
+export const user = {username: "Alice", age: 30}
+
+// 기본 모듈 내보내기
+export default{
+  version: '1.0',
+  creator: "Minji Choi",
+}
