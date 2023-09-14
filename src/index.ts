1f67838de3146b8fcb87d2c39265c20d5ccf494e
Minji Choi <minj22222c@gmail.com>
2023-09-14 17:51:30 +0900

    모듈번들러

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// ecmascipt(ex, js) module
+/*
+import구문은
+구조분해할당
+const {username, age} = {username: "Alice", age: 30}
+// const person = {username: "Alice", age: 30} -> person.username / person.age
+*/
+
+
+
+import { appName, greet, user } from "./module";
+
+// 디폴트 모듈
+// 자동완성으로 파일명하고 모듈명을 동일하게 해줌.
+// import module from "./module";
+import metadata from "./module";
+
+
+
+const name = "Javascript!!!";
+console.log(greet(`${name}-${appName}-${metadata.version}-${metadata.creator}-${user.username}-${user.age}`));
+document.getElementById("root").innerHTML = greet(`${name}-${appName}-${metadata.version}-${metadata.creator}-${user.username}-${user.age}`);
